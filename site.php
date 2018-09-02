3ea46f3353d5e14ac5e3f4ed226f49b96a8e4a01
Norberto Mazuhy Gertge Junior <nmgjunior@ucs.br>
2018-09-02 19:21:41 -0300

    Produtos vs. Categorias

--- site.php
+++ site.php
@@ -1,6 +1,7 @@
 <?php
 
 use \Hcode\Page;
+use \Hcode\Model\Category;
 use \Hcode\Model\Product;
 
 $app->get('/', function() {
@@ -21,7 +22,7 @@ $app->get("/categories/:idcategory", function($idcategory){
     $page=new Page();
     $page->setTpl("category", [
         'category'=>$category->getValues(),
-        'products'=> []
+        'products'=> Product::checkList($category->getProducts())
     ]);
     
 });
