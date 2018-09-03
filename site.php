020d1835815adc29e944b0efbf9540e6e81ebefb
Norberto Mazuhy Gertge Junior <nmgjunior@ucs.br>
2018-09-02 21:29:12 -0300

    Paginação produtos

--- site.php
+++ site.php
@@ -15,16 +15,23 @@ $app->get('/', function() {
 });
 
 $app->get("/categories/:idcategory", function($idcategory){
-
-    $category = new Category();
-    $category->get((int)$idcategory);
-    
-    $page=new Page();
-    $page->setTpl("category", [
-        'category'=>$category->getValues(),
-        'products'=> Product::checkList($category->getProducts())
-    ]);
-    
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+	$category = new Category();
+	$category->get((int)$idcategory);
+	$pagination = $category->getProductsPage($page);
+	$pages = [];
+	for ($i=1; $i <= $pagination['pages']; $i++) { 
+		array_push($pages, [
+			'link'=>'/categories/'.$category->getidcategory().'?page='.$i,
+			'page'=>$i
+		]);
+	}
+	$page = new Page();
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'products'=>$pagination["data"],
+		'pages'=>$pages
+	]);
 });
 
 ?>
