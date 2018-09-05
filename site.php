3ddb50392b95f6d1062c46ebb65e97d84f87887a
Norberto Mazuhy Gertge Junior <nmgjunior@ucs.br>
2018-09-05 16:39:24 -0300

    Frete

--- site.php
+++ site.php
@@ -50,9 +50,11 @@ $app->get("/cart", function(){
     $cart= Cart::getFromSession();
     
     $page = new Page();
+
     $page->setTpl("cart", [
         'cart'=>$cart->getValues(),
-        'products'=>$cart->getProducts()
+        'products'=>$cart->getProducts(),
+        'error'=>Cart::getMsgError()
     ]);
     
 });
@@ -94,5 +96,15 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
     header("Location: /cart"); 
     exit;
 });
+
+$app->post("/cart/freight", function(){
+    $cart =  Cart::getFromSession();
+    $cart->setFreight($_POST['zipcode']);
+    header("Location: /cart");
+    exit;
+
+});
+
+
 
 ?>
