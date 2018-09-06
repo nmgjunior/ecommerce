aa70c3bd413694b80a7e6ce4e41688ba139ad286
Norberto Mazuhy Gertge Junior <nmgjunior@ucs.br>
2018-09-06 16:44:07 -0300

    Esqueceu senha - usuário

--- site.php
+++ site.php
@@ -195,4 +195,66 @@ $app->post("/register", function(){
 });
 
 
+
+
+$app->get("/forgot", function(){
+        $page = new Page(); 
+   
+   $page->setTpl("forgot");
+});
+
+$app->post("/forgot", function(){
+
+    
+    $user = User::getForgot($_POST["email"], false);
+    
+    header("Location:/forgot/sent");
+    
+    exit;
+});
+
+$app->get("/forgot/sent", function(){
+        $page = new Page(); 
+   
+   $page->setTpl("forgot-sent");
+    
+});
+
+$app->get("/forgot/reset", function(){
+    $user = User::validForgotDecrypt($_GET["code"]);
+        if ($user==0){
+            header("Location: /forgot");
+            exit;
+        }
+    $page = new Page();
+    $page->setTpl("forgot-reset", array(
+        "name"=>$user["desperson"],
+        "code"=>$_GET["code"]
+    ));
+});
+
+$app->post("/forgot/reset", function(){
+
+    $forgot = User::validForgotDecrypt($_POST["code"]);
+    if ($forgot==0){
+        header("Location: /forgot");
+        exit;
+    }
+    User::setForgotUsed($forgot["idrecovery"]);
+    
+    $user = new User();
+    
+    $user->get((int)$forgot["iduser"]);
+    
+    $password=password_hash($_POST["password"], PASSWORD_DEFAULT, ['cost'=>12]);
+    
+    $user->setPassword($password);
+    
+    $page = new Page(); 
+   
+   $page->setTpl("forgot-reset-success");
+});
+
+
+
 ?>
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -152,7 +152,7 @@ public static function login($login, $password)
         
     }
     
-    public static function getForgot($email)
+    public static function getForgot($email, $inadmin=true)
     {
         $sql = new Sql();
         
@@ -181,7 +181,12 @@ public static function login($login, $password)
                 $iv= random_bytes(openssl_cipher_iv_length('aes-256-cbc'));
                 $code = openssl_encrypt($dataRecovery['idrecovery'],'aes-256-cbc', User::SECRET,0,$iv);
                 $result= base64_encode($iv.$code);
-                $link="http://www.hcodecommerce.com.br/admin/forgot/reset?code=$result";
+
+                if($inadmin==true){
+                    $link="http://www.hcodecommerce.com.br/admin/forgot/reset?code=$result";
+                } else {
+                    $link="http://www.hcodecommerce.com.br/forgot/reset?code=$result";
+                }
                 
                 $mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir Senha da Hcode Store", "forgot", array(
 					"name"=>$data["desperson"],
