4e4b0f42dc57778ef7d70644b7cb2d927c2173d3
Norberto Mazuhy Gertge Junior <nmgjunior@ucs.br>
2018-09-13 10:41:58 -0300

    Profile

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -13,6 +13,7 @@ class User extends Model {
     const SECRET = "hcodephp7_secret";
     const ERROR = "UserError";
     const ERROR_REGISTER = "UserErrorRegister";
+    const SUCCESS = "UserSuccess";
     
     public static function getFromSession(){
         $user = new User();
@@ -252,16 +253,37 @@ public static function login($login, $password)
     {
         $_SESSION[User::ERROR] = $msg;
     }
+
     public static function getError()
     {
         $msg = (isset($_SESSION[User::ERROR]) && $_SESSION[User::ERROR]) ? $_SESSION[User::ERROR] : '';
         User::clearError();
         return $msg;
     }
+
     public static function clearError()
     {
         $_SESSION[User::ERROR] = NULL;
     }
+
+    public static function setSuccess($msg)
+    {
+        $_SESSION[User::SUCCESS] = $msg;
+    }
+
+    public static function getSuccess()
+    {
+        $msg = (isset($_SESSION[User::SUCCESS]) && $_SESSION[User::SUCCESS]) ? $_SESSION[User::SUCCESS] : '';
+        User::clearSuccess();
+        return $msg;
+    }
+    
+    public static function clearSuccess()
+    {
+        $_SESSION[User::SUCCESS] = NULL;
+    }
+
+
 
 
     public static function setErrorRegister($msg)
